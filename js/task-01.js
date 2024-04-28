843f8ce6566d6527dc26e4a4e9c7b994b49fcd6a
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-04-28 17:05:39 +0200

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,9 @@
-// const categoriesList = document.querySelector("#categories");
+const categoriesList = document.querySelector("#categories");
 
-// console.log(`Number of categories: ${categoriesList.children.length}`);
-// [...categoriesList.children].forEach((item) => {
-//   console.log(`Category: ${item.firstElementChild.textContent}`);
-//   console.log(`Elements: ${item.lastElementChild.children.length}`);
-// });
+console.log(`Number of categories: ${categoriesList.children.length}`);
+
+[...categoriesList.children].forEach((item) => {
+  console.log(item.lastElementChild.children);
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
