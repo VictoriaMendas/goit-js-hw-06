024c62f6983adc54adaf2cd7d9f2afe50c2be4e2
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-05-29 09:02:40 +0200

    css

--- js/task-01.js
+++ js/task-01.js
@@ -6,3 +6,14 @@ console.log(`Number of categories: ${categoriesList.children.length}`);
   console.log(`Category: ${item.firstElementChild.textContent}`);
   console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
+
+function getExtremeScores(scores) {
+  console.log({ worst: Math.min(...scores) });
+}
+
+getExtremeScores([89, 64, 42, 17, 93, 51, 26]);
+
+const getUserWithEmail = (users, email) => {
+  users.find((user) => user.email === email);
+  return users.name;
+};
