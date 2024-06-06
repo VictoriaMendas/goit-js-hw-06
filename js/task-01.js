bb767b4080337b2222f97bd28bc0d4df97989606
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-06-06 13:09:05 +0200

    finished style.css

--- js/task-01.js
+++ js/task-01.js
@@ -17,3 +17,30 @@ const getUserWithEmail = (users, email) => {
   users.find((user) => user.email === email);
   return users.name;
 };
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    rating: 7.94,
+  },
+  {
+    title: "Enemy of God",
+    author: "Bernard Cornwell",
+    rating: 8.67,
+  },
+];
